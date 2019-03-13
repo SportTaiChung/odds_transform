951723f54d943719fe2123668ee35c5d8421a257
user <user@Macde-MacBook-Pro.local>
2019-03-13 15:08:34 +0800

    新增美棒大小

--- testBsFunctionP.py
+++ testBsFunctionP.py
@@ -1,6 +1,7 @@
 import APHDC_pb2 
 import copy
 import testBSzf
+import testBSds
 
 
 def baseball(Data):
@@ -13,24 +14,49 @@ def baseball(Data):
         awayL = bs.usZF.awayZF.line
         homeO = bs.usZF.homeZF.odds
         awayO = bs.usZF.awayZF.odds
+
+
+        ## 讓分
         zfBS = testBSzf.calBSzf(gameType,homeL,awayL,homeO,awayO,homeDe,awayDe)
         bs.twZF.homeZF.line=zfBS[0]
         bs.twZF.awayZF.line=zfBS[1]
+        ## 如果獨贏為0 讓分獨贏都關 因為算是需要讓分及獨贏缺一不可
         if homeDe == '0' : 
             bs.twZF.homeZF.odds="0"
             bs.twZF.awayZF.odds="0" 
             bs.de.home='0'
             bs.de.away='0'
         else :
-            bs.twZF.homeZF.odds="0.95"
-            bs.twZF.awayZF.odds="0.95"  
-            bs.de.home=zfBS[4]
-            bs.de.away=zfBS[5]
+            bs.de.home=zfBS[2]
+            bs.de.away=zfBS[3]
+            if homeO == '0' :
+                bs.twZF.homeZF.odds="0"
+                bs.twZF.awayZF.odds="0"
+            else :
+                bs.twZF.homeZF.odds="0.95"
+                bs.twZF.awayZF.odds="0.95"  
 
+
+        ## 一輸二贏 (美盤讓分盤口減一)
         if homeO != '0' :
             bs.esre.let = 1 if "-" in homeL else 2
-            bs.esre.home = zfBS[2]
-            bs.esre.away = zfBS[3]
+            bs.esre.home = zfBS[4]
+            bs.esre.away = zfBS[5]
+
+        ## 大小
+        line = bs.usDS.line
+        over = bs.usDS.over
+        under = bs.usDS.under
+        dsBS = testBSds.calBSds(line,over,under)
+        bs.twDS.line=dsBS
+        if over ==  '0' or over ==  "0.0" :
+            bs.twDS.over="0"
+            bs.twDS.under="0"
+        else :
+            bs.twDS.over="0.94"
+            bs.twDS.under="0.94"
+
+        
 
         sendData.append(copy.deepcopy(bs))                
 
