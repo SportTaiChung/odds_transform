3a92138fea1ffff518725ed2e256a08ccebe67ff
apple <apple@mail.js-tech.tw>
2019-03-16 01:53:24 +0800

    更改 含加時賽只有全場換算

--- testHcFunctionP.py
+++ testHcFunctionP.py
@@ -24,7 +24,7 @@ def hockey(Data):
             notNHLData = enData.aphdc
             for no in notNHLData :
                 no
-            sendData.append(copy.deepcopy(no))
+                sendData.append(copy.deepcopy(no))
         elif '總得分' in hc.information.league :
             # print(hc.information.league)
             notNHL.append(hc)
@@ -34,9 +34,10 @@ def hockey(Data):
             notNHLData = enData.aphdc
             for no in notNHLData :
                 no
-            sendData.append(copy.deepcopy(no))
+                sendData.append(copy.deepcopy(no))
         else:
             gameType = hc.game_type
+            gameClass = hc.game_class
             homeL = hc.usZF.homeZF.line
             awayL = hc.usZF.awayZF.line
             homeO = hc.usZF.homeZF.odds
@@ -44,56 +45,57 @@ def hockey(Data):
             homeDe = hc.de.home
             awayDe = hc.de.away
             # zfBS = testHCzf.calBSzf(gameType,homeL,awayL,homeO,awayO,homeDe,awayDe)
-            zfBS = testBSzf.calBSzf(gameType,homeL,awayL,homeO,awayO,homeDe,awayDe)
-            hc.twZF.homeZF.line=zfBS[0]
-            hc.twZF.awayZF.line=zfBS[1]
-            
-            ## 如果獨贏為0 讓分獨贏都關 因為算是需要讓分及獨贏缺一不可
-            if homeDe == '0' : 
-                hc.twZF.homeZF.odds="0"
-                hc.twZF.awayZF.odds="0" 
-                hc.de.home='0'
-                hc.de.away='0'
-            else :
-                hc.de.home=zfBS[2]
-                hc.de.away=zfBS[3]
-                if homeO == '0' :
+            if 'full' in gameType :
+                zfBS = testBSzf.calBSzf(gameType,homeL,awayL,homeO,awayO,homeDe,awayDe)
+                hc.twZF.homeZF.line=zfBS[0]
+                hc.twZF.awayZF.line=zfBS[1]
+                
+                ## 如果獨贏為0 讓分獨贏都關 因為算是需要讓分及獨贏缺一不可
+                if homeDe == '0' : 
                     hc.twZF.homeZF.odds="0"
-                    hc.twZF.awayZF.odds="0"
+                    hc.twZF.awayZF.odds="0" 
+                    hc.de.home='0'
+                    hc.de.away='0'
                 else :
-                    hc.twZF.homeZF.odds="0.95"
-                    hc.twZF.awayZF.odds="0.95"  
+                    hc.de.home=zfBS[2]
+                    hc.de.away=zfBS[3]
+                    if homeO == '0' :
+                        hc.twZF.homeZF.odds="0"
+                        hc.twZF.awayZF.odds="0"
+                    else :
+                        hc.twZF.homeZF.odds="0.95"
+                        hc.twZF.awayZF.odds="0.95"  
 
 
-            ## 一輸二贏 (美盤讓分盤口減一)
-            if homeO != '0' :
-                hc.esre.let = 1 if "-" in homeL else 2
-                hc.esre.home = zfBS[4]
-                hc.esre.away = zfBS[5]
+                ## 一輸二贏 (美盤讓分盤口減一)
+                if homeO != '0' :
+                    hc.esre.let = 1 if "-" in homeL else 2
+                    hc.esre.home = zfBS[4]
+                    hc.esre.away = zfBS[5]
 
-            ## 大小
-            line = hc.usDS.line
-            over = hc.usDS.over
-            under = hc.usDS.under
-            # if over == "0" or over == "0.0":
-            #     hc.twDS.line= '0+0'
-            #     hc.twDS.over= '0'
-            #     hc.twDS.under= '0'
-            # else :
-            #     dsBS = testCutOneP.cutOne(over,under)
-            #     if '.5' in str(float(line)) :
-            #         line = line
-            #     else :
-            #         line = str(float(line)).replace('.0','+0')
-            #     hc.twDS.line= line
-            #     hc.twDS.over= dsBS[0]
-            #     hc.twDS.under= dsBS[1]
-            dsBS = testBSds.calBSds(line,over,under)
-            hc.twDS.line= dsBS
-            hc.twDS.over= '0.94'
-            hc.twDS.under= '0.94'
+                ## 大小
+                line = hc.usDS.line
+                over = hc.usDS.over
+                under = hc.usDS.under
+                if over == "0" or over == "0.0":
+                    hc.twDS.line= '0+0'
+                    hc.twDS.over= '0'
+                    hc.twDS.under= '0'
+                else :
+                    dsBS = testCutOneP.cutOne(over,under)
+                    if '.5' in str(float(line)) :
+                        line = line
+                    else :
+                        line = str(float(line)).replace('.0','+0')
+                    hc.twDS.line= line
+                    hc.twDS.over= dsBS[0]
+                    hc.twDS.under= dsBS[1]
+                # dsBS = testBSds.calBSds(gameClass,line,over,under)
+                # hc.twDS.line= dsBS
+                # hc.twDS.over= '0.94'
+                # hc.twDS.under= '0.94'
        
-            sendData.append(copy.deepcopy(hc))                
+                sendData.append(copy.deepcopy(hc))                
 
     # print(sendData)
     datas = APHDC_pb2.ApHdcArr()
