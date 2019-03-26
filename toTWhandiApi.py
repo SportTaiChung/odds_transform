a6afc0fb5b6566b5d79753638424f071c6b7b3ea
apple <apple@mail.js-tech.tw>
2019-03-26 17:38:31 +0800

    線上使用版

--- toTWhandiApi.py
+++ toTWhandiApi.py
@@ -5,17 +5,13 @@ from flask import request
 import json
 import requests
 import sendMQ
+import APHDC_pb2
 import testHcFunctionP
 import testBskFunctionP
 import testBsFunctionP
 import testCutOneP
 import testBeforeSeason
- 
-import bsFunctionP
-import bskFunctionP
-import cutOneP
-import APHDC_pb2
-
+import testBsMixFunction
 
 
 app = Flask(__name__)
@@ -23,12 +19,11 @@ CORS(app)
 
 @app.route('/')
 def hello():
-
     return 'Start to Trans ! '
 
 
 @app.route('/transWithProtobuf', methods=['GET', 'POST'])
-def func3():
+def trans():
 
     data = request.get_data()
     enData = APHDC_pb2.ApHdcArr()
@@ -37,34 +32,19 @@ def func3():
     ## 球賽對應function
     for en in Data :
         game = en.game_class
-    ## 測試用    
-    if 'testPs' == en.source  or 'CMD' == en.source:
-        if 'basketball' in game :
-            out = testBskFunctionP.basketball(Data)
-        elif 'football' in game :
-            out = testCutOneP.justCutOne_fun(Data)   
-        elif 'soccer' in game :
-            out = testCutOneP.justCutOne_fun(Data)   
-        elif 'hockey'  in game :
-            out = testHcFunctionP.hockey(Data)
-        elif 'mlb' in game :
-            out = testBsFunctionP.baseball(Data)
-            
-    ## 正式用     
-    else :
-        if 'basketball' in game :
-            out = testBskFunctionP.basketball(Data)
-            # out = bskFunctionP.basketball_fun(Data)
-        elif 'football' in game :
-            out = testCutOneP.justCutOne_fun(Data) 
-        elif 'hockey'  in game :
-            out = testHcFunctionP.hockey(Data)
-            # out = bsFunctionP.baseball_fun(Data)
-        elif 'mlb'  in game :
-            out = testBeforeSeason.seasonMLB(Data) 
-        # elif 'mlb' or 'npb' or 'cpbl' or  'kbo' or 'hockey' in game :
-        #     out = bsFunctionP.baseball_fun(Data)
 
+    if 'basketball' in game :
+        out = testBskFunctionP.basketball(Data)
+    elif 'football' in game :
+        out = testCutOneP.justCutOne_fun(Data) 
+    elif 'soccer' in game :
+        out = testCutOneP.justCutOne_fun(Data)
+    elif 'hockey'  in game : 
+        out = testHcFunctionP.hockey(Data)
+    elif 'mlb'  in game :
+        out = testBsMixFunction.baseballMix(Data)
+    # elif 'mlb' or 'npb' or 'cpbl' or  'kbo' or 'hockey' in game :
+    #     out = bsFunctionP.baseball_fun(Data)
 
     
     outData = APHDC_pb2.ApHdcArr()
@@ -76,28 +56,31 @@ def func3():
         ouc = ou.game_class
         outype = ou.game_type
         twzf = ou.twZF.homeZF.line
-        if ous == 'PS38':
+        if 'test' in ous :
+            ous=ous.replace('test','')
             if 'hockey' == ouc:
-                que = 'PS38_HC'
+                que = 'test_'+ous+'_HC'
             elif 'football' == ouc :
-                que = 'PS38_FB'
+                que = 'test_'+ous+'_FB'
             elif 'basketball' in ouc :
-                if 'full' in outype :
-                    que = 'PS38_BK'
-                else :
-                    que = 'PS38_OBK'
-            elif 'mlb' or 'npb' or 'cpbl' or  'kbo' in ouc:
-                que = 'PS38_BS'
-        elif ous == 'CMD':
-            if 'basketball' in ouc:
-                que = 'CMD_BK'
-            elif 'hockey' == ouc:
-                que = 'CMD_HC'
+                que = 'test_'+ous+'_BK'
+            elif 'mlb' in ouc:
+                que = 'test_'+ous+'_BS'
             elif 'soccer' == ouc :
-                que = 'CMD_SC'
-        elif 'test' in ous :
-            que = 'Apple'
-        
+                que = 'test_'+ous+'_SC'            
+        else:
+            if 'hockey' == ouc:
+                que = ous+'_HC'
+            elif 'football' == ouc :
+                que = ous+'_FB'
+            elif 'basketball' in ouc :
+                que = ous+'_BK'
+            elif 'mlb' in ouc:
+                que = ous+'_BS'
+            elif 'soccer' == ouc :
+                que = ous+'_SC'
+
+
     # sendMQ.send_MQ(out,'Apple','rabbit.avia520.com','AE86', '200p')
     sendMQ.send_MQ(out,que,'10.0.1.198','GTR', '565p')
     return out
@@ -107,5 +90,3 @@ if __name__ == '__main__':
     # app.run(host='127.0.0.1', port=5004, debug=True ,threaded=True)
     app.run(host='0.0.0.0', port=5004, debug=True ,threaded=True)
 
-
-   
