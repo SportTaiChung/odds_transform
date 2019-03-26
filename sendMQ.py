2cd5538322aa200ebbba24e324ab1d9d75e0119f
apple <apple@mail.js-tech.tw>
2019-03-26 17:25:58 +0800

    新增telegram機器人

--- sendMQ.py
+++ sendMQ.py
@@ -1,13 +1,25 @@
 import pika
-
-def split_list(data):   # 分成一次送50筆
+import telegram
+import configparser
+# 分成一次送50筆
+def split_list(data):   
 
     limit = 50
-
     return [data[i:i + limit] for i in range(0, len(data), limit)]
 
 
+# telegram 機器人 
+def telegramBot(message): 
 
+    conf = configparser.ConfigParser()  
+    conf.read("config.ini")
+    token = conf["telegram"]['token']
+    id = conf['telegram']['chat_id']
+    bot = telegram.Bot(token=token)
+    message = message
+    bot.send_message(chat_id=id, text=message) 
+
+# 傳送資料到ＭＱ
 def send_MQ(DATA,que,host,user,ps):     #資料送rabbitMQ
 
     # cred = pika.PlainCredentials('AE86', '200p')
@@ -24,8 +36,8 @@ def send_MQ(DATA,que,host,user,ps):     #資料送rabbitMQ
     )
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    channel.basic_publish(exchange='',
-                          routing_key=que,
+    channel.basic_publish(exchange=que,
+                          routing_key='',
                           body=DATA
                           )
     # print("sent")
