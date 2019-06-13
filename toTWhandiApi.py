d5382c8dd5691829aaf25d6346a4e6c7ddc541d2
apple <apple@mail.js-tech.tw>
2019-06-13 18:30:24 +0800

    更改host

--- toTWhandiApi.py
+++ toTWhandiApi.py
@@ -75,5 +75,5 @@ def trans():
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5004, debug=True, threaded=True)
-    # app.run(host='0.0.0.0', port=5004, debug=True, threaded=True)
+    # app.run(host='127.0.0.1', port=5004, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5004, debug=True, threaded=True)
