18015d8b2aa69d95ca8e08cf72e2ea5fe3ec59f7
apple <apple@mail.js-tech.tw>
2019-08-02 17:20:42 +0800

    timeout  3 秒

--- sendMQ.py
+++ sendMQ.py
@@ -22,17 +22,12 @@ def telegramBot(message):
 # 傳送資料到ＭＱ的交換機
 def send_MQ(DATA, que, host, user, pw):     #資料送rabbitMQ
 
-    # cred = pika.PlainCredentials('AE86', '200p')
-    # cred = pika.PlainCredentials('GTR', '565p')  #正式
     cred = pika.PlainCredentials(user, pw)
     params = pika.ConnectionParameters(
-        # host = '10.0.1.198',
-        # host = 'rmq.nba1688.net',
-        # host = 'rabbit.avia520.com',
         host=host,
         virtual_host='/',
         credentials=cred,
-        socket_timeout=6
+        socket_timeout=3
     )
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
@@ -42,22 +37,3 @@ def send_MQ(DATA, que, host, user, pw):     #資料送rabbitMQ
                           )
     connection.close()
 
-
-# 傳ＭＱ的que
-def hkMQ(DATA, que, host, user, pw, port):     #資料送rabbitMQ
-
-    cred = pika.PlainCredentials(user, pw)
-    params = pika.ConnectionParameters(
-        port=port,
-        host=host,
-        virtual_host='/',
-        credentials=cred,
-        socket_timeout=3
-    )
-    connection = pika.BlockingConnection(params)
-    channel = connection.channel()
-    channel.basic_publish(exchange='',
-                          routing_key=que,
-                          body=DATA
-                          )
-    connection.close()
