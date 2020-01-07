ab0f3c03ea407d8a99ae52c9c270b04d22b08575
apple <apple@mail.js-tech.tw>
2020-01-07 16:11:34 +0800

    fix

--- toTWhandiApi.py
+++ toTWhandiApi.py
@@ -45,32 +45,31 @@ def trans():
             errorfile.write(str(data)+'\n'+str(e)+'\n'+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n')
             errorfile.close()
 
-        sportMap ={
-            'mlb':'_BS',
-            'npb':'_BS',
-            'kbo':'_BS',
-            'hockey':'_HC',
-            'football':'_FB',
-            'basketball':'_BK',
-            'otherbasketball':'_BK',
-            'soccer':'_SC'
-        }
-        outData = APHDC_noDB_pb2.ApHdcArr()
-        outData.ParseFromString(out)
-        Data = outData.aphdc
-        ## 來源對應que
-        for ou in Data:
-            ous = ou.source
-            ouc = ou.game_class
-            que = ous + sportMap.get(ouc)
-
+        # sportMap ={
+        #     'mlb':'_BS',
+        #     'npb':'_BS',
+        #     'kbo':'_BS',
+        #     'hockey':'_HC',
+        #     'football':'_FB',
+        #     'basketball':'_BK',
+        #     'otherbasketball':'_OBK',
+        #     'soccer':'_SC'
+        # }
+        # outData = APHDC_noDB_pb2.ApHdcArr()
+        # outData.ParseFromString(out)
+        # Data = outData.aphdc
+        # ## 來源對應que
+        # for ou in Data:
+        #     ous = ou.source
+        #     ouc = ou.game_class
+        #     que = ous + sportMap.get(ouc)
+    
         # sendMQ.send_MQ(out, 'test_CMD', 'rabbit.avia520.com', 'AE86', '200p', 5672)
         sendMQ.send_MQ(out, 'test_PS38', '192.168.1.201', 'GTR', '565p', 5672)
         return out
 
     except Exception as e:
-        telegramBot(str(e))
-
+        print(str(e))
 
 if __name__ == '__main__':
     # app.run(host='127.0.0.1', port=5004, debug=True, threaded=True)
