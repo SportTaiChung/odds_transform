46a002fb044503090b847fe1aed7e853efdcd2f8
apple <apple@mail.js-tech.tw>
2020-01-20 12:39:05 +0800

    fix 不回傳資料

--- toTWhandiApi.py
+++ toTWhandiApi.py
@@ -37,39 +37,40 @@ def trans():
                 out = testCutOneP.justCutOne_fun(Data)
             elif game in ('mlb', 'npb', 'kbo'):
                 out = newBSMixFunction.baseballMix(Data)
+
+
+            # sportMap ={
+            #     'mlb':'_BS',
+            #     'npb':'_BS',
+            #     'kbo':'_BS',
+            #     'hockey':'_HC',
+            #     'football':'_FB',
+            #     'basketball':'_BK',
+            #     'otherbasketball':'_OBK',
+            #     'soccer':'_SC',
+            #     'tennis':'_TN'
+            # }
+            # outData = APHDC_noDB_pb2.ApHdcArr()
+            # outData.ParseFromString(out)
+            # Data = outData.aphdc
+            # ## 來源對應que
+            # for ou in Data:
+            #     ous = ou.source
+            #     ouc = ou.game_class
+            #     que = ous + sportMap.get(ouc)
+        
+            # sendMQ.send_MQ(out, 'test_CMD', 'rabbit.avia520.com', 'AE86', '200p', 5672)
+            sendMQ.send_MQ(out, 'test_PS38', 'rtmcq.nba1688.net', 'GTR', '565p', 5672)
+            return 'Success !!'
         except Exception as e:
-            errorfile = open('error.log','a')
+            errorfile = open('error.log', 'a')
             errorfile.write(str(data)+'\n'+str(e)+'\n'+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n')
             errorfile.close()
-
-
-        # sportMap ={
-        #     'mlb':'_BS',
-        #     'npb':'_BS',
-        #     'kbo':'_BS',
-        #     'hockey':'_HC',
-        #     'football':'_FB',
-        #     'basketball':'_BK',
-        #     'otherbasketball':'_OBK',
-        #     'soccer':'_SC',
-        #     'tennis':'_TN'
-        # }
-        # outData = APHDC_noDB_pb2.ApHdcArr()
-        # outData.ParseFromString(out)
-        # Data = outData.aphdc
-        # ## 來源對應que
-        # for ou in Data:
-        #     ous = ou.source
-        #     ouc = ou.game_class
-        #     que = ous + sportMap.get(ouc)
-    
-        # sendMQ.send_MQ(out, 'test_CMD', 'rabbit.avia520.com', 'AE86', '200p', 5672)
-        sendMQ.send_MQ(out, 'test_PS38', 'rtmcq.nba1688.net', 'GTR', '565p', 5672)
-        return out
-
+            return 'Error !!'
 
     except Exception as e:
         print(str(e))
+        return 'Error !!'
 
 if __name__ == '__main__':
     # app.run(host='127.0.0.1', port=5004, debug=True, threaded=True)
