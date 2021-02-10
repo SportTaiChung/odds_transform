dadce8e29ad6ce24ec66c880d31adc566fd2a3b6
sy <sy@mail.js-tech.tw>
2021-02-10 13:34:45 +0800

    fix: remove pidfile in config

--- gun.py
+++ gun.py
@@ -1,14 +1,12 @@
 import multiprocessing
 
-bind = '127.0.0.1:5004'
+bind = '0.0.0.0:5004'
 workers = multiprocessing.cpu_count() * 2 + 1
 backlog = 128
 worker_class = "gevent"
 daemon = True
 timeout = 60
 keepalive = 5
-proc_name = 'gunicorn.proc'
-pidfile = '/tmp/gunicorn.pid'
 logfile = '/home/pyuser/log/odds_transform_api.log'
 debug = True
 loglevel = 'debug'
